c8966d0e08631ee4afbc012020c8a8e67290be22
Gerhard Schlager <gerhard.schlager@discourse.org>
2021-12-27 01:45:55 +0100

    Update build config to detect build errors

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,22 @@
-import dts from "rollup-plugin-dts";
-import esbuild from "rollup-plugin-esbuild";
+import typescript from "@rollup/plugin-typescript";
 
 const name = require("./package.json").main.replace(/\.js$/, "");
 
-const bundle = (config) => ({
-  ...config,
+export default {
   input: "src/index.ts",
-  external: (id) => !/^[./]/.test(id),
-});
-
-export default [
-  bundle({
-    plugins: [esbuild()],
-    output: [
-      {
-        file: `${name}.js`,
-        format: "cjs",
-        exports: "auto",
-        sourcemap: true,
-      },
-      {
-        file: `${name}.mjs`,
-        format: "es",
-        exports: "auto",
-        sourcemap: true,
-      },
-    ],
-  }),
-  bundle({
-    plugins: [dts()],
-    output: {
-      file: `${name}.d.ts`,
-      format: "es",
+  output: [
+    {
+      file: `${name}.js`,
+      format: "cjs",
+      exports: "auto",
+      sourcemap: true,
     },
-  }),
-];
+    {
+      file: `${name}.mjs`,
+      format: "es",
+      exports: "auto",
+      sourcemap: true,
+    },
+  ],
+  plugins: [typescript()],
+};
